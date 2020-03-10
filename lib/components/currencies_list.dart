1b0f1b04fe6de40649c1d0b0088deecc2080ce20
Boris Kayi <boriskayienzo@gmail.com>
2020-03-11 01:35:11 +0200

    Make curencies lists dynamic

--- lib/components/currencies_list.dart
+++ lib/components/currencies_list.dart
@@ -4,68 +4,50 @@ import 'package:flutter/material.dart';
 class CurrenciesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-        CurrencyCard(),
-      ],
-    );
-  }
-}
-
-class CurrencyCard extends StatelessWidget {
-  const CurrencyCard({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          leading: Container(
-            height: 60,
-            width: 60,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.cyan),
-              borderRadius: BorderRadius.circular(30)
-            ),
-            child: CircleAvatar(
-              radius: 60,
-              child: Text(
-                'BTC',
-                style: Theme.of(context).textTheme.title,
+    return ListView.builder(
+      itemCount: fiatList.length,
+      itemBuilder: (context, index) {
+        return Column(
+          children: <Widget>[
+            ListTile(
+              leading: Container(
+                height: 60,
+                width: 60,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.cyan),
+                    borderRadius: BorderRadius.circular(30)),
+                child: CircleAvatar(
+                  radius: 60,
+                  child: Text(
+                    '${fiatLogos[index]}',
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  backgroundColor: CoinColors.bluredDarkBlue,
+                ),
               ),
-              backgroundColor: CoinColors.bluredDarkBlue,
+              title: Text(
+                '${fiatList.values.toList()[index]}',
+                style: Theme.of(context)
+                    .textTheme
+                    .subhead
+                    .copyWith(color: Colors.blueGrey),
+              ),
             ),
-          ),
-          title: Text(
-            'Bitcoin (BTC)',
-            style: Theme.of(context)
-                .textTheme
-                .subhead
-                .copyWith(color: Colors.blueGrey),
-          ),
-          subtitle: Text(
-            '\$12356789',
-            style: Theme.of(context)
-                .textTheme
-                .display1
-                .copyWith(color: Colors.white),
-          ),
-        ),
-        Divider(color: Color(0xFF182A56), thickness: .65)
-      ],
+            Divider(color: Color(0xFF182A56), thickness: .65)
+          ],
+        );
+      },
     );
   }
 }
+
+// ListTile(
+
+//           subtitle: Text(
+//             '\$12356789',
+//             style: Theme.of(context)
+//                 .textTheme
+//                 .display1
+//                 .copyWith(color: Colors.white),
+//           ),
+//         ),
--- lib/constants.dart
+++ lib/constants.dart
@@ -5,87 +5,40 @@ class CoinColors {
   static Color bluredDarkBlue = Color(0xFF0D1E4C);
 }
 
-List<Text> fiatList = kCurrenciesList.map(
-  (fiat) {
-    String fiatName;
-    if (fiat == 'AUD') {
-      fiatName = 'Australian Dollar';
-    } else if (fiat == 'BRL') {
-      fiatName = 'Brazilian Real';
-    } else if (fiat == 'CAD') {
-      fiatName = 'Canadian Dollar';
-    } else if (fiat == 'CNY') {
-      fiatName = 'Yuan';
-    } else if (fiat == 'EUR') {
-      fiatName = 'Euro';
-    } else if (fiat == 'GBP') {
-      fiatName = 'Pound';
-    } else if (fiat == 'HKD') {
-      fiatName = 'Hong Kong Dollar';
-    } else if (fiat == 'IDR') {
-      fiatName = 'Rupiah';
-    } else if (fiat == 'ILS') {
-      fiatName = 'Israeli Shekel';
-    } else if (fiat == 'INR') {
-      fiatName = 'Indian Rupee';
-    } else if (fiat == 'JPY') {
-      fiatName = 'Yen';
-    } else if (fiat == 'MXN') {
-      fiatName = 'Peso';
-    } else if (fiat == 'NOK') {
-      fiatName = 'Krone';
-    } else if (fiat == 'NZD') {
-      fiatName = 'New Zeland Dollar';
-    } else if (fiat == 'PLN') {
-      fiatName = 'Polish Zloty';
-    } else if (fiat == 'RON') {
-      fiatName = 'Romanian Leu';
-    } else if (fiat == 'RUB') {
-      fiatName = 'Ruble';
-    } else if (fiat == 'SEK') {
-      fiatName = 'Swedish Krona';
-    } else if (fiat == 'SGD') {
-      fiatName = 'Singapore Dollar';
-    } else if (fiat == 'USD') {
-      fiatName = 'US Dollar';
-    } else if (fiat == 'ZAR') {
-      fiatName = 'Rand';
-    }
-    return Text(
-      '$fiatName',
-      style: TextStyle(fontSize: 20, color: Colors.white),
-    );
-  },
-).toList();
+Map<String, String> fiatList = {
+  'AUD': 'Australian Dollar',
+  'BRL': 'Brazilian Real',
+  'CAD': 'Canadian Dollar',
+  'CNY': 'Chinese Yuan',
+  'EUR': 'Euro',
+  'GBP': 'GB Pound',
+  'HKD': 'Hong Kong Dollar',
+  'IDR': 'Indonesian Rupiah',
+  'ILS': 'Israeli Shekel',
+  'INR': 'Indian Rupee',
+  'JPY': 'Japanese Yen',
+  'MXN': 'Mexican Peso',
+  'NOK': 'Norwegian Krone',
+  'NZD': 'New Zeland Dollar',
+  'PLN': 'Polish Zloty',
+  'RON': 'Romanian Leu',
+  'RUB': 'Russian Ruble',
+  'SEK': 'Swedish Krona',
+  'SGD': 'Singapore Dollar',
+  'USD': 'US Dollar',
+  'ZAR': 'South African Rand',
+  'RWF': 'Rwandan Francs',
+};
 
-const List<String> cryptoList = [
-  'BTC',
-  'ETH',
-  'LTC',
-];
-const List<String> kCurrenciesList = [
-  'AUD',
-  'BRL',
-  'CAD',
-  'CNY',
-  'EUR',
-  'GBP',
-  'HKD',
-  'IDR',
-  'ILS',
-  'INR',
-  'JPY',
-  'MXN',
-  'NOK',
-  'NZD',
-  'PLN',
-  'RON',
-  'RUB',
-  'SEK',
-  'SGD',
-  'USD',
-  'ZAR'
-];
+const Map<String, String> cryptoList = {
+  'BTC': 'Bitcoin',
+  'ETH': 'Etheurem',
+  'LTC': 'Litecoin',
+};
+
+List<String> fiatLogos = fiatList.keys.toList();
+List<String> cryptoLogos = cryptoList.keys.toList();
+
 
 String addParams(List paramsList) {
   String param = '';
@@ -96,6 +49,6 @@ String addParams(List paramsList) {
 }
 
 String api =
-    'https://min-api.cryptocompare.com/data/pricemulti?fsyms=${addParams(kCurrenciesList)}&tsyms=${addParams(cryptoList)}&api_key=';
+    'https://min-api.cryptocompare.com/data/pricemulti?fsyms=${addParams(fiatLogos)}&tsyms=${addParams(cryptoLogos)}&api_key=';
 const String apiKey =
     'bf6595bcb4e7084aa0228031e51c0b848c316b6857a848b73f7700ee6ba381b7';
